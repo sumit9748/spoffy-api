94b171f7fdb4f2443cdff1cd02c8a97a7654c1fc
sumit mondal <sumitmondal9448@gmail.com>
2024-12-25 14:42:46 +0530

    initialSetUp

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
 const BASE_URL = "https://spoffy-api.onrender.com/connect/";
+const DEV_URL = "http://localhost:8000/connect/"
 
-const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const user = JSON.parse(localStorage.getItem("persist:root"))?.spoffyUser;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: DEV_URL,
 });
 
 export const userRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: DEV_URL,
   header: { token: `Bearer ${TOKEN}` },
 });
